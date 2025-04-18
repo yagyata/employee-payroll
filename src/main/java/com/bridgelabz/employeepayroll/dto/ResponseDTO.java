13af291ae9f7e3cb668085db4ff75b2735ab4d5c
Yagyata <yagyatasharmaa@gmail.com>
2025-04-18 10:22:26 +0530

    Updated ResponseDTO

--- src/main/java/com/bridgelabz/employeepayroll/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/ResponseDTO.java
@@ -1,25 +1,30 @@
 package com.bridgelabz.employeepayroll.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ResponseDTO {
     private String message;
     private Object data;
 
-    public ResponseDTO (String message, Object data) {
-        this.message = message;
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-    public Object getData() {
-        return data;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    public void setData(Object data) {
-        this.data = data;
-    }
+//    public ResponseDTO (String message, Object data) {
+//        this.message = message;
+//        this.data = data;
+//    }
+//
+//    public String getMessage() {
+//        return message;
+//    }
+//    public Object getData() {
+//        return data;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+//    public void setData(Object data) {
+//        this.data = data;
+//    }
 }
